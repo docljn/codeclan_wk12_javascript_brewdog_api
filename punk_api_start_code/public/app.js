6abfd73f5f1654ddc6714b8a095b11534431afb5
DocLJN <ljnoble@gmail.com>
2018-02-21 22:48:10 +0000

    ingredient list displayed

--- punk_api_start_code/public/app.js
+++ punk_api_start_code/public/app.js
@@ -18,14 +18,13 @@ const carryOutAPIRequest = function () {
   if(this.status !== 200) return;
   const jsonString = this.responseText;
   const beers = JSON.parse(jsonString);
-  console.log("API Request retrieved beers");
-  console.log(beers);
-
   displayBeers(beers);
 };
 
 const makeBeerListing = function (beer) {
   const ulOuter = document.getElementById("beers-ul");
+  ulOuter.className += " flex";
+
   const liOuter = document.createElement("li");
   liOuter.innerText = beer.name;
   liOuter.className += " beer_details";
@@ -35,27 +34,43 @@ const makeBeerListing = function (beer) {
 
   const beerImage = document.createElement("img");
   beerImage.src = beer.image_url;
-  beerImage.setAttribute("width", 60);
+  beerImage.setAttribute("height", 90);
   beerImage.setAttribute("alt", "A picture of " + beer.name);
 
   const liInnerSecond = document.createElement("li");
   liInnerSecond.className += " ingredients";
-  liInnerSecond.innerText = "Malts";
+  liInnerSecond.innerText = "Ingredients";
   beer.ingredients.malt[0].name;
 
   const liInnerThird = document.createElement("li");
   liInnerThird.className += " malts";
+  liInnerThird.className += " flex";
+  liInnerThird.className += " flex-vertical"
+
+  const divmalts = document.createElement("div");
+  const divhops = document.createElement("div");
+  const divyeast = document.createElement("div");
 
   const maltsListArray = beer.ingredients.malt;
-
-  const maltsNamesList = maltsListArray.reduce ( function (list, malt ) {
-    list += (malt.name + "\n");
-    return list;
-  },"");
-
-  liInnerThird.innerText = maltsNamesList;
+  const maltsNamesArray = getNames(maltsListArray);
+  const uniqueMalts = [...new Set(maltsNamesArray)];
+  const maltsNamesList = uniqueMalts.join(", ");
+  divmalts.innerText = "Malts: " + maltsNamesList;
 
 
+  const hopsListArray = beer.ingredients.hops;
+  const hopsNamesArray = getNames(hopsListArray);
+  const uniqueHops = [...new Set(hopsNamesArray)];
+
+  const hopsNamesList = uniqueHops.join(", ");
+  divhops.innerText = "Hops: " + hopsNamesList;
+
+  const yeastList = beer.ingredients.yeast;
+  divyeast.innerText = "Yeast: " + yeastList;
+
+  liInnerThird.appendChild(divmalts);
+  liInnerThird.appendChild(divhops);
+  liInnerThird.appendChild(divyeast);
 
   liInner.appendChild(beerImage);
   ulInner.appendChild(liInner);
@@ -66,11 +81,21 @@ const makeBeerListing = function (beer) {
   ulOuter.appendChild(liOuter);
 };
 
+const getNames = function (array) {
+  let newArray = [];
+  array.forEach( function (item) {
+    newArray.push(item.name);
+  });
+  return newArray;
+};
+
 const displayBeers = function (beers) {
   beers.forEach(function (beer) {
     makeBeerListing(beer);
   });
 };
+
+
 
 
 
